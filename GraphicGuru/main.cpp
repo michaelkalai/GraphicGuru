4c923af09c1fe063ab6904314bf9d3949686a3e6
michaelkalai <michael.kalai124@gmail.com>
2023-05-08 19:40:22 -0400

    Moved objects instantiated in main into other classes

--- GraphicGuru/main.cpp
+++ GraphicGuru/main.cpp
@@ -9,20 +9,18 @@ using namespace std;
 using namespace sf;
 int main()
 {
-	int width, height, channels;
-	string filename = "images/test_image1.jpg";
-	Color red = Color::Red;
+	// constructs window
 	Color bg(78, 102, 102);
-	ImageManipulator image(filename);
 
-	RenderWindow window(VideoMode(VideoMode::getDesktopMode().width, VideoMode::getDesktopMode().height), "SFML works!", Style::Fullscreen);
+	RenderWindow window(VideoMode(VideoMode::getDesktopMode().width, VideoMode::getDesktopMode().height), "GraphicGuru", Style::Fullscreen);
 	window.setFramerateLimit(60);
 
+	// loads font
 	Font font;
 	font.loadFromFile("./font/AovelSansRounded.ttf");
 	UI ui(font);
 
-
+	// program functions until closed
 	while (window.isOpen())
 	{
 		Event event;
@@ -30,21 +28,20 @@ int main()
 		{
 			if (event.type == Event::Closed)
 				window.close();
+			// handles event when mouse button is pressed
 			else if (event.type == Event::MouseButtonPressed) {
 				if (event.mouseButton.button == Mouse::Left) {
 					if (ui.buttonclicked(event.mouseButton.x, event.mouseButton.y)) {
-						cout << "saving file" << endl;
-						image.savefile();
-						cout << "save complete" << endl;
+						cout << "button function finished" << endl;
 					}
-					else if (image.getspriteptr()->getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
-						image.colorpixels(event.mouseButton.x, event.mouseButton.y, event.mouseButton.x, event.mouseButton.y, red);
+					else if (ui.imageclicked(event.mouseButton.x, event.mouseButton.y)) {
+						cout << "image colored" << endl;
 					}
 				}
 			}
 		}
+		// displays program to user
 		window.clear(bg);
-		image.draw(window);
 		ui.draw(window);
 		window.display();
 	}
