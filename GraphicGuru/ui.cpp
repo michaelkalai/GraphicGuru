15bb74e75869eb4377667720d0b5555852614cf2
michaelkalai <michael.kalai124@gmail.com>
2023-05-08 19:39:18 -0400

    Modified class construction

--- GraphicGuru/ui.cpp
+++ GraphicGuru/ui.cpp
@@ -1,17 +1,22 @@
 #include "ui.h"
 
+// constructs buttons and adds a basic image (for testing)
 UI::UI(Font& font)
 {
 	generatefilebtn(font);
+	addimage();
 }
 
+// draws all buttons and the selected image
 void UI::draw(RenderWindow& window)
 {
 	for (int i = 0; i < _buttons.size(); i++) {
 		_buttons[i]->draw(window);
 	}
+	_selimage->draw(window);
 }
 
+// returns true if mouse position is within a button when clicked
 bool UI::buttonclicked(int& x, int& y)
 {
 	for (int i = 0; i < _buttons.size(); i++) {
@@ -22,13 +27,41 @@ bool UI::buttonclicked(int& x, int& y)
 	return false;
 }
 
+// generates file drop down button and buttons within it
 void UI::generatefilebtn(Font& font)
 {
+	UI* ui = &*this;
 	string text = "File";
 	float btnwidth = 100;
 	float btnheight = 50;
-	TextMenuButton* btnptr = new TextMenuButton(btnwidth, btnheight, 0, 0, "File", font);
-	btnptr->addbutton(new TextMenuButton(btnwidth, btnheight, 0, 0, "M1BTN", font));
+	TextMenuButton* btnptr = new TextMenuButton(nullptr, nullptr, ui, btnwidth, btnheight, 0, 0, "File", font);
+	btnptr->addbutton(new TextButton(_funch, &FunctionHandler::save, ui, btnwidth, btnheight, 0, 0, "Save", font));
 	btnptr->alignmenu();
 	_buttons.push_back(btnptr);
+}
+
+// loads additional image
+void UI::addimage()
+{
+	string filename = "images/test_image1.jpg";
+	_images.push_back(new ImageManipulator(filename));
+	_selimage = _images[0];
+}
+
+// colors pixels if image sprite is clicked
+bool UI::imageclicked(int& x, int& y)
+{
+
+	if (_selimage->getspriteptr()->getGlobalBounds().contains(x, y)) {
+		Color red = Color::Red;
+		_selimage->colorpixels(x, y, x, y, red);
+		return true;
+	}
+	return false;
+}
+
+// returns pointer to selected image
+ImageManipulator* UI::getselimageptr()
+{
+	return _selimage;
 }
--- GraphicGuru/ui.h
+++ GraphicGuru/ui.h
@@ -1,19 +1,28 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include "forward.h"
 #include "button.h"
 #include "textbutton.h"
 #include "textmenubutton.h"
+#include "imagemanipulator.h"
+#include "functionhandler.h"
 
 using namespace std;
 
 class UI
 {
 	vector<Button*> _buttons;
+	vector<ImageManipulator*> _images;
+	ImageManipulator* _selimage;
+	FunctionHandler* _funch;
 public:
 	UI(Font& font);
 	void draw(RenderWindow& window);
 	bool buttonclicked(int& x, int& y);
 	void generatefilebtn(Font& font);
+	void addimage();
+	bool imageclicked(int& x, int& y);
+	ImageManipulator* getselimageptr();
 };
 
